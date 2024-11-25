939709244293e8a510166a83fb37f99e14c63817
v1690117 <vladimir.veselov@phystech.edu>
2024-11-26 01:25:33 +0300

    make service return 401 instead of redirects

--- src/main/java/com/v1690117/pnt/service/config/WebSecurityConfig.java
+++ src/main/java/com/v1690117/pnt/service/config/WebSecurityConfig.java
@@ -3,11 +3,12 @@ package com.v1690117.pnt.service.config;
 import com.v1690117.pnt.service.auth.CustomOAuth2UserService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 
 @Configuration
 @EnableWebSecurity
@@ -19,7 +20,7 @@ public class WebSecurityConfig {
                                 .anyRequest().authenticated()
                 )
                 .exceptionHandling(e -> e
-                        .authenticationEntryPoint(new LoginUrlAuthenticationEntryPoint("/login"))
+                        .authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED))
                 )
                 .logout(l -> l
 //                        .logoutUrl("/logout")
