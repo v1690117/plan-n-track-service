0d6c83ba1a1d81f3eaabcd6549608b666540a649
v1690117 <vladimir.veselov@phystech.edu>
2025-01-05 15:39:56 +0300

    configure local development

---
+++ src/main/java/com/v1690117/pnt/service/Constants.java
@@ -0,0 +1,6 @@
+package com.v1690117.pnt.service;
+
+public final class Constants {
+    public static final String PROFILE_PRODUCTION = "production";
+    public static final String PROFILE_NOT_PRODUCTION = "!production";
+}
---
+++ src/main/java/com/v1690117/pnt/service/service/UserService.java
@@ -0,0 +1,7 @@
+package com.v1690117.pnt.service.service;
+
+import com.v1690117.pnt.service.model.User;
+
+public interface UserService {
+    User getCurrentUser();
+}
